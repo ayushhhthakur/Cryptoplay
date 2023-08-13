e1565129518f969c4474a602d21f114fc83f751a
Ayush Thakur <ayushthakur1412@gmail.com>
2023-08-13 21:38:01 +0530

    updated

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,66 @@
-import React from 'react'
-import styled from "styled-components";
+import React, { useState } from 'react';
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-export default Login
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Here you can add the logic for sending the login data to your server
+    // using the fetch API or an HTTP library like axios.
+    // Remember to perform proper error handling and validation.
+
+    // For example, using the fetch API:
+    fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data); // You can handle the response data here
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
