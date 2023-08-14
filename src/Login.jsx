47dbbd23ee783edbc6acda2d88ce79dc86e7a1c2
Ayush Thakur <ayushthakur1412@gmail.com>
2023-08-14 15:13:58 +0530

    updated

--- src/Login.jsx
+++ src/Login.jsx
@@ -16,9 +16,8 @@ const Wrapper = styled.section`
   margin-top: 50px;
   margin-bottom: 50px;
 }
-.content a {
+.content h1 a {
   text-decoration: underline;
-  color black;
 }
 `
 
@@ -27,7 +26,7 @@ const Login = () => {
     <Wrapper>
       <div className="content">
         <h2>Connect your Tezos Wallet</h2>
-        <h1>Don't have a wallet?<br />Click <a target="_blank" href="https://tezos.com/create-wallet/">Here</a></h1>
+        <h1>Don't have a wallet?<br />Click <a target="_blank" href="https://tezos.com/create-wallet/" style={{color: 'black'}}>Here.</a></h1>
         {/* <h1>Click <Link to="https://tezos.com/create-wallet/">Here</Link></h1> */}
       </div>
       <div className="btn-position">
